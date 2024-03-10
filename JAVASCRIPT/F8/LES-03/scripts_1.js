3d3492aa3fa31c58cea3b1ed286e862aaad89309
Nakio204 <fedorra.2004@gmail.com>
2024-03-10 20:20:12 +0700

    um

---
+++ JAVASCRIPT/F8/LES-03/scripts_1.js
@@ -0,0 +1,10 @@
+// Toan tu so sanh
+
+/**
+ * === : So sánh giá trị và kiểu dữ liệu bằng nhau 
+ * !== : So sánh giá trị và kiểu dữ liệu khác nhau 
+ */
+
+var a = 5;
+var b = 6;
+console.log(a === b);
